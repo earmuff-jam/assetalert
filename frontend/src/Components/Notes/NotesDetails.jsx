c5f98d50c71af68d52c7cac4484a4ac953dc3309
Mohit <mohit.paudyal@gmail.com>
2024-03-22 15:12:28 -0500

    add minor css changes

--- frontend/src/Components/Notes/NotesDetails.jsx
+++ frontend/src/Components/Notes/NotesDetails.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextComponent from '../../stories/TextComponent/TextComponent';
 import { DeleteRounded, EditRounded, ExpandMoreRounded } from '@material-ui/icons';
-import { Accordion, AccordionDetails, AccordionSummary, Box, Chip, IconButton, Typography } from '@material-ui/core';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Chip, IconButton } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
