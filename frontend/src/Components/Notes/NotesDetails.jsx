b34d9d9859773fc3ca0ff69812e5feaaa81695a0
Mohit <mohit.paudyal@gmail.com>
2024-03-22 15:11:50 -0500

    add component for notes and notes details

--- frontend/src/Components/Notes/NotesDetails.jsx
+++ frontend/src/Components/Notes/NotesDetails.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Accordion, AccordionDetails, AccordionSummary, Box, IconButton, Typography } from '@material-ui/core';
-import { DeleteRounded, EditRounded, ExpandMoreRounded } from '@material-ui/icons';
 import TextComponent from '../../stories/TextComponent/TextComponent';
+import { DeleteRounded, EditRounded, ExpandMoreRounded } from '@material-ui/icons';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Chip, IconButton, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
+    marginBottom: theme.spacing(1),
   },
   content: {
     padding: theme.spacing(1),
-    boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.5)',
+    boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+  },
+  contentContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(2),
   },
   rowContainer: {
     display: 'flex',
@@ -23,73 +28,107 @@ const useStyles = makeStyles((theme) => ({
     fontSize: theme.typography.pxToRem(15),
     fontWeight: theme.typography.fontWeightRegular,
   },
+  textVariant: {
+    fontWeight: 'bold',
+  },
+  colorVariant: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+  extraGap: {
+    gap: theme.spacing(1),
+  },
 }));
 
 const NotesDetails = () => {
   const classes = useStyles();
 
+  const notesData = [
+    {
+      id: 1,
+      category: 'Recently added notes',
+      totalNotes: 2,
+      details: [
+        {
+          id: 1,
+          note_title: 'Plants code from home depot',
+          note_description: 'Bought a coupon code to use while buying plants at Lowes or Home Depot',
+          updated_by: 'xxArthr',
+          updated_at: '',
+        },
+        {
+          id: 1,
+          note_title: 'Planted Magnolia according to sketch and diagram.',
+          note_description:
+            'Had an issue where the plant magnolia was not getting proper sunlight and therefore we had to move it to a better place. Thank god my wife sketched the location out and we planted it. Now we want to track a watering schedule.',
+          updated_by: 'xxArthr',
+          updated_at: '',
+        },
+      ],
+    },
+    {
+      id: 2,
+      category: 'Last Week',
+      totalNotes: 32,
+      details: [
+        {
+          id: 1,
+          note_title: 'Plants code from home depot',
+          note_description: 'Bought a coupon code to use while buying plants at Lowes or Home Depot',
+          updated_by: 'xxArthr',
+          updated_at: '',
+        },
+        {
+          id: 1,
+          note_title: 'Planted Magnolia according to sketch and diagram.',
+          note_description:
+            'Had an issue where the plant magnolia was not getting proper sunlight and therefore we had to move it to a better place. Thank god my wife sketched the location out and we planted it. Now we want to track a watering schedule.',
+          updated_by: 'xxArthr',
+          updated_at: '',
+        },
+      ],
+    },
+  ];
+
   return (
     <div className={classes.root}>
-      <Accordion>
-        <AccordionSummary expandIcon={<ExpandMoreRounded />} aria-controls="panel1a-content" id="panel1a-header">
-          <Typography className={classes.heading}>Recently added notes</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Box className={classes.content}>
-            <Box className={classes.rowContainer}>
-              <TextComponent
-                value={'Plants code from home depot - last edited 2 hours ago by test@gmail.com'}
-                gutterBottom={true}
-                loading={false}
-                textStyle={classes.text}
-              />
-              <Box className={classes.emptyGap}></Box>
-              <IconButton>
-                <DeleteRounded />
-              </IconButton>
-              <IconButton>
-                <EditRounded />
-              </IconButton>
+      {notesData.map((v) => (
+        <Accordion elevation={0} className={classes.colorVariant}>
+          <AccordionSummary expandIcon={<ExpandMoreRounded />} aria-controls="panel1a-content" id="panel1a-header">
+            <Box className={[classes.rowContainer, classes.extraGap].join(' ')}>
+              <TextComponent textStyle={classes.heading} value={v.category} loading={false} />
+              <Chip label={v.totalNotes} size="small" />
             </Box>
-            <Box>
-              <Typography>
-                Bought a coupon code to use while buying plants at Lowes or Home Depot. - last edited 2 hours ago by
-                test@gmail.com
-              </Typography>
-              <Typography>
-                Bought a coupon code to use while buying plants at Lowes or Home Depot. - last edited 2 hours ago by
-                test@gmail.com
-              </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Box className={classes.contentContainer}>
+              {v.details.map((note) => (
+                <Box className={classes.content}>
+                  <Box className={classes.rowContainer}>
+                    <TextComponent
+                      value={note.note_title}
+                      gutterBottom={true}
+                      loading={false}
+                      textStyle={[classes.text, classes.textVariant].join(' ')}
+                    />
+                    <Box className={classes.emptyGap}></Box>
+                    <IconButton>
+                      <DeleteRounded />
+                    </IconButton>
+                    <IconButton>
+                      <EditRounded />
+                    </IconButton>
+                  </Box>
+                  <Box>
+                    <TextComponent textStyle={classes.heading} value={note.note_description} loading={false} />
+                    <TextComponent textStyle={classes.heading} value={note.updated_by} loading={false} />
+                    <TextComponent textStyle={classes.heading} value={note.updated_at} loading={false} />
+                  </Box>
+                </Box>
+              ))}
             </Box>
-          </Box>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary expandIcon={<ExpandMoreRounded />} aria-controls="panel2a-content" id="panel2a-header">
-          <Typography className={classes.heading}>Last week</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Box>
-            <Typography>
-              Bought a coupon code to use while buying plants at Lowes or Home Depot. - last edited 2 hours ago by
-              test@gmail.com
-            </Typography>
-            <Typography>
-              Bought a coupon code to use while buying plants at Lowes or Home Depot. - last edited 2 hours ago by
-              test@gmail.com
-            </Typography>
-            <Typography>
-              Bought a coupon code to use while buying plants at Lowes or Home Depot. - last edited 2 hours ago by
-              test@gmail.com
-            </Typography>
-          </Box>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion disabled>
-        <AccordionSummary expandIcon={<ExpandMoreRounded />} aria-controls="panel3a-content" id="panel3a-header">
-          <Typography className={classes.heading}>Last Month</Typography>
-        </AccordionSummary>
-      </Accordion>
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </div>
   );
 };
