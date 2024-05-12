57b52f9a179ffdc04116de35e67bdb9c15778b09
Mohit <mohit.paudyal@gmail.com>
2024-05-12 16:03:50 -0500

    Add checks to prevent profanity in chat message
    - update package.json file to add client checks for profanity. using
      common package bad-words
    - update readme to support nvm upgrade if necessary
    - add filter for community message to block profane words as much as
      possible
    - add .env file support for webservice messages. in terms of tls and
      https we might have to think how to handle this
    
    closes #154

--- frontend/src/util/Common.js
+++ frontend/src/util/Common.js
@@ -4,11 +4,15 @@
  *
  * clientConfig stores all required keys for application
  *
+ * react environment host for application
+ * react websocket host for application
+ * geocoding api for server - using earmuffjam
+ *
  */
 
-// react local development user url
 export const REACT_APP_LOCALHOST_URL = import.meta.env?.REACT_APP_LOCALHOST_URL || 'http://localhost:8087';
-// geocoding api earmuff jam user geocoding api key
+export const REACT_APP_LOCALHOST_URL_SOCKET_BASE_URL =
+  import.meta.env?.REACT_APP_LOCALHOST_URL_SOCKET_BASE_URL || 'ws://localhost:8087';
 export const REACT_APP_GEOCODING_MAP_API_KEY = import.meta.env?.GEOCODING_MAP_API_KEY || '';
 
 /**
