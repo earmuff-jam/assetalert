6e7f632135ad46ab7458ca5d32721020a0a6908d
mohit <mohit.paudyal@gmail.com>
2024-07-28 08:50:26 -0500

    Add changes to support muiv5 migration
    - updated recent activities component to muiv5
    - cleanup on components since we are not using participation and
    volunteering for asset tracking purposes.

--- frontend/src/features/RecentList/RecentActivities.jsx
+++ frontend/src/features/RecentList/RecentActivities.jsx
@@ -1,12 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import RecentActivity from './RecentActivity';
 import { ExpandMoreRounded } from '@mui/icons-material';
 import { Accordion, AccordionDetails, AccordionSummary, Box, Skeleton, Typography } from '@mui/material';
 import { DisplayNoMatchingRecordsComponent } from '../common/utils';
-
+import { useEffect } from 'react';
+import { profileActions } from '../Profile/profileSlice';
 
 const RecentActivities = () => {
+  const dispatch = useDispatch();
   const { loading, recentActivities } = useSelector((state) => state.profile);
+
+  useEffect(() => {
+  dispatch(profileActions.getRecentActivitiesList());
+  }, []);
 
   if (loading) {
     return <Skeleton width={`calc(100% - 1rem)`} height={'20rem'} />;
@@ -19,7 +25,7 @@ const RecentActivities = () => {
         {recentActivities.map((event, index) => (
           <Accordion key={index} elevation={0}>
             <AccordionSummary expandIcon={<ExpandMoreRounded />}>
-              <Typography variant="body1">{event.title}</Typography>
+              <Typography variant="h6">{event.title}</Typography>
             </AccordionSummary>
             <AccordionDetails>
               <RecentActivity key={index} index={index} activity={event} />
--- frontend/src/features/RecentList/RecentList.jsx
+++ frontend/src/features/RecentList/RecentList.jsx
@@ -4,8 +4,12 @@ import RecentActivities from './RecentActivities';
 const RecentList = () => {
   return (
     <Stack>
-      <Typography variant="h5" gutterBottom>
+      <Typography variant="h5">
         Recent Activities
+      </Typography>
+      <Typography variant="caption" gutterBottom>
+        View all of your recent activities here. You can view all created and / or updated asset(s). Track changes for
+        the last ten assets.
       </Typography>
       <RecentActivities />
     </Stack>
