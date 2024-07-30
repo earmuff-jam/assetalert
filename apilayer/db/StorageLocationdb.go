66edf4551fc7ad56cbf4b0d55c17d5f45697ca91
mohit <mohit.paudyal@gmail.com>
2024-07-30 18:25:58 -0500

    fix add vs edit category issue
    restructure for asset alert
    
    Address changes in backend and migration for muiv5
    
    - add changes for notification handler
    we don't have that sql atm so removing all relative go code for it
    
    - added cleanup for a lot of ui code
    
    Revert "Address changes in backend and migration for muiv5"
    
    This reverts commit a5a9f5233f1a3198584540f3fbf8692cd8ef3a8e.
    
    Revert "restructure for asset alert"
    
    This reverts commit 8598d908b5312c226c82c8cbc7187a67f235f1b2.
    
    Limit categories to 4 items in home page
    
    Remove all unused api calls for backend service
    
    - removed events api calls
    - removed notifications api calls
    - reformatted unit test and files

---
+++ apilayer/db/StorageLocationdb.go
@@ -0,0 +1,126 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+	"github.com/mohit2530/communityCare/model"
+)
+
+// RetrieveAllStorageLocation ...
+func RetrieveAllStorageLocation(user string) ([]model.StorageLocation, error) {
+	db, err := SetupDB(user)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	sqlStr := "SELECT id, location, created_at, created_by, updated_at, updated_by, sharable_groups FROM community.storage_locations"
+	rows, err := db.Query(sqlStr)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var data []model.StorageLocation
+
+	var storageLocationID sql.NullString
+	var storageLocation sql.NullString
+	var createdBy sql.NullString
+	var createdAt sql.NullTime
+	var updatedBy sql.NullString
+	var updatedAt sql.NullTime
+	var sharableGroups pq.StringArray
+
+	for rows.Next() {
+		var ec model.StorageLocation
+		if err := rows.Scan(&storageLocationID, &storageLocation, &createdAt, &createdBy, &updatedAt, &updatedBy, &sharableGroups); err != nil {
+			return nil, err
+		}
+
+		ec.ID = storageLocationID.String
+		ec.Location = storageLocation.String
+		ec.CreatedAt = createdAt.Time
+		ec.CreatedBy = createdBy.String
+		ec.UpdatedAt = updatedAt.Time
+		ec.UpdatedBy = updatedBy.String
+		ec.SharableGroups = sharableGroups
+
+		data = append(data, ec)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// DeleteStorageLocation
+func DeleteStorageLocation(user string, locationID string) error {
+	db, err := SetupDB(user)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	sqlStr := `DELETE FROM community.storage_locations WHERE id=$1`
+	_, err = db.Exec(sqlStr, locationID)
+	if err != nil {
+		log.Printf("unable to delete event ID %+v", locationID)
+		return err
+	}
+	return nil
+}
+
+// addNewStorageLocation ...
+//
+// adds new storage location if not existing but if there was an existing storage location, we just return that ID
+func addNewStorageLocation(user string, draftLocation string, created_by string, emptyLocationID *string) error {
+	db, err := SetupDB(user)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	fetchSqlStr := `SELECT count(sl.id), sl.id FROM community.storage_locations sl WHERE sl.location = $1 GROUP BY sl.id`
+	var count int
+	err = db.QueryRow(fetchSqlStr, draftLocation).Scan(&count, emptyLocationID)
+	if err != nil {
+		log.Printf("found existing location for selected item. using existing location for %+v", draftLocation)
+	}
+
+	// save new storage location if it does not already exists
+	if count == 0 {
+		sqlStr := `INSERT INTO community.storage_locations(location, created_by, updated_by, created_at, updated_at, sharable_groups) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`
+
+		var locationID string
+		var sharableGroups = make([]uuid.UUID, 0)
+
+		userID, err := uuid.Parse(created_by)
+		if err != nil {
+			return err
+		}
+		sharableGroups = append(sharableGroups, userID)
+
+		err = db.QueryRow(
+			sqlStr,
+			draftLocation,
+			created_by,
+			created_by,
+			time.Now(),
+			time.Now(),
+			pq.Array(sharableGroups),
+		).Scan(&locationID)
+
+		if err != nil {
+			return err
+		}
+		*emptyLocationID = locationID
+	}
+
+	return nil
+}
---
+++ apilayer/handler/StorageLocationHandler_test.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/mohit2530/communityCare/config"
+	"github.com/mohit2530/communityCare/db"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_GetStorageLocationHealthCheck(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/api/v1/health", nil)
+	w := httptest.NewRecorder()
+	GetStorageLocationHealthCheck(w, req, config.CTO_USER)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	assert.True(t, len(string(data)) <= 69)
+	assert.Equal(t, 200, res.StatusCode)
+}
+
+func Test_GetAllStorageLocations(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/api/v1/locations", nil)
+	w := httptest.NewRecorder()
+	db.PreloadAllTestVariables()
+	GetAllStorageLocations(w, req, config.CTO_USER)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	assert.Equal(t, 200, res.StatusCode)
+	assert.Greater(t, len(data), 0)
+	t.Logf("response = %+v", string(data))
+
+	w = httptest.NewRecorder()
+	GetAllStorageLocations(w, req, config.CEO_USER)
+	res = w.Result()
+	assert.Equal(t, 400, res.StatusCode)
+	assert.Equal(t, "400 Bad Request", res.Status)
+}
