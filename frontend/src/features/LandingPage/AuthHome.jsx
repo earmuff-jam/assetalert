d66f1d769b94fc3e8d0cab9a4dcb96e2cfd32658
mohit <mohit.paudyal@gmail.com>
2024-08-28 16:36:06 -0500

    fix lint issues

--- frontend/src/features/LandingPage/AuthHome.jsx
+++ frontend/src/features/LandingPage/AuthHome.jsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from '@mui/material';
+import { Stack } from '@mui/material';
 import StyledAppBar from './StyledAppBar';
 import Authenticator from './Authenticator';
 import InviteSection from './InviteSection';
