9d439de20b8e2978ab1e6463494efb40b68377a1
mohit <mohit.paudyal@gmail.com>
2024-09-27 18:09:04 -0500

    remove ability to support file upload

--- setup/_adjust_perm_local_uploads.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIRECTORY=./local_uploads
-
-USER_UID=$(id -u)
-USER_GID=$(id -g)
-
-sudo chown -R ${USER_UID}:${USER_GID} $DIRECTORY
-echo "Changed ownership of $DIRECTORY to $USER_UID:$USER_GID"
