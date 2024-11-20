f252ce2302d0feba55a490fc8a96ef785ba39740
mohit paudyal <mohit.paudyal@gmail.com>
2024-11-19 18:29:52 -0600

    Add changes to better style add item form
    - Added changes to support better styling for add item
    form, but all in all, it still feels like its missing some feature.
    I would like to table this and visit this at a later date. Honestly, it
    seems like the stepper is too much work for something as simple as
    this.

--- frontend/src/features/InventoryList/AddInventory/AddInventoryFormDetails/AddInventoryFormReview.jsx
+++ frontend/src/features/InventoryList/AddInventory/AddInventoryFormDetails/AddInventoryFormReview.jsx
@@ -4,31 +4,45 @@ import { Button, Card, CardActions, CardContent, IconButton, Stack, Typography }
 export default function AddInventoryFormReview({ formData, handleReset, handleSubmit }) {
   return (
     <Stack alignItems="center">
-      <Card sx={{ display: 'flex', width: '100%', maxWidth: '600px' }}>
-        <CardContent>
-          <Stack direction="row">
-            <IconButton disabled>
-              <BookmarkRounded color={formData.is_bookmarked.value ? 'primary' : 'secondary'} />
-            </IconButton>
-            <Stack>
-              <Typography variant="h6">{formData.name.value}</Typography>
-              <Typography variant="caption">{formData.description.value}</Typography>
-              <Stack direction="row" spacing={1} alignItems="center">
-                <Typography fontWeight="bold">Qty: </Typography>
-                <Typography variant="caption">{formData.quantity.value} item</Typography>
-              </Stack>
-            </Stack>
+      <Stack direction="row" alignSelf={'flex-start'}>
+        <IconButton disabled>
+          <BookmarkRounded color={formData.is_bookmarked.value ? 'primary' : 'secondary'} />
+        </IconButton>
+        <Stack spacing={1} justifyContent={'space-between'}>
+          <Stack direction={'row'} spacing={1} alignItems={'center'}>
+            <Typography variant="subtitle2" color="text.secondary">
+              {formData.name.label}
+            </Typography>
+            <Typography variant="subtitle2" color="text.secondary">
+              {formData.name.value}
+            </Typography>
           </Stack>
-          <CardActions>
-            <Button startIcon={<RestartAltRounded />} onClick={handleReset}>
-              Reset
-            </Button>
-            <Button startIcon={<CheckRounded />} onClick={handleSubmit}>
-              Submit
-            </Button>
-          </CardActions>
-        </CardContent>
-      </Card>
+          <Stack direction={'row'} spacing={1} alignItems={'center'}>
+            <Typography variant="subtitle2" color="text.secondary">
+              {formData.description.label}
+            </Typography>
+            <Typography variant="subtitle2" color="text.secondary">
+              {formData.description.value}
+            </Typography>
+          </Stack>
+          <Stack direction={'row'} spacing={1} alignItems={'center'}>
+            <Typography variant="subtitle2" color="text.secondary">
+              {formData.quantity.label}
+            </Typography>
+            <Typography variant="subtitle2" color="text.secondary">
+              {formData.quantity.value}
+            </Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+      <Stack direction={'row'} alignSelf={'flex-end'}>
+        <Button startIcon={<RestartAltRounded />} onClick={handleReset}>
+          Reset
+        </Button>
+        <Button startIcon={<CheckRounded />} onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Stack>
     </Stack>
   );
 }
