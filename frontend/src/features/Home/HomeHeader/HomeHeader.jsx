25d0123744dff4419caa3522ea48ec299d828f70
mohit paudyal <mohit.paudyal@gmail.com>
2024-11-30 16:36:01 -0600

    Add tooltip and cleanup for homepage and homeHeader.jsx files

--- frontend/src/features/Home/HomeHeader/HomeHeader.jsx
+++ frontend/src/features/Home/HomeHeader/HomeHeader.jsx
@@ -1,27 +1,14 @@
-import dayjs from 'dayjs';
-
-import { Card, CardContent, IconButton, Stack, Skeleton, Typography } from '@mui/material';
 import { CategoryRounded, EngineeringRounded, WarningRounded } from '@mui/icons-material';
+import { Card, CardContent, IconButton, Stack, Skeleton, Typography, Tooltip } from '@mui/material';
 
 import RowHeader from '../../../common/RowHeader';
 import { pluralizeWord } from '../../../common/utils';
 
-export default function HomeHeader({ categories = [], maintenancePlans = [], assets = [] }) {
-  const totalPastDueItems = assets.filter((v) => dayjs(v.returntime).isBefore(dayjs())).length || 0;
-  const totalItemsUnderCategory = categories.reduce((acc, el) => {
-    if (el.items && el.items[0] != '') {
-      acc += el.items.length;
-    }
-    return acc;
-  }, 0);
-
-  const totalItemsUnderMaintenancePlans = maintenancePlans.reduce((acc, el) => {
-    if (el.items && el.items[0] != '') {
-      acc += el.items.length;
-    }
-    return acc;
-  }, 0);
-
+export default function HomeHeader({
+  assetsUnderCategories = [],
+  assetsUnderMaintenancePlans = [],
+  assetsPastDue = [],
+}) {
   return (
     <>
       <RowHeader title="Asset Summary" />
@@ -31,7 +18,8 @@ export default function HomeHeader({ categories = [], maintenancePlans = [], ass
             label="assigned categories"
             icon={<CategoryRounded />}
             color="primary"
-            dataLabel={totalItemsUnderCategory}
+            tooltipTitle={assetsUnderCategories.flatMap((v) => v.items).join(', ')}
+            dataLabel={assetsUnderCategories.flatMap((v) => v.items).length}
             loading={false}
           />
         </CardItem>
@@ -40,7 +28,8 @@ export default function HomeHeader({ categories = [], maintenancePlans = [], ass
             label="assigned maintenance plan"
             icon={<EngineeringRounded />}
             color="primary"
-            dataLabel={totalItemsUnderMaintenancePlans}
+            tooltipTitle={assetsUnderMaintenancePlans.flatMap((v) => v.items).join(', ')}
+            dataLabel={assetsUnderMaintenancePlans.flatMap((v) => v.items).length}
             loading={false}
           />
         </CardItem>
@@ -49,7 +38,8 @@ export default function HomeHeader({ categories = [], maintenancePlans = [], ass
             label="past return deadline"
             icon={<WarningRounded />}
             color="error"
-            dataLabel={totalPastDueItems}
+            tooltipTitle={assetsPastDue.join(', ')}
+            dataLabel={assetsPastDue.length}
             loading={false}
           />
         </CardItem>
@@ -64,24 +54,26 @@ const CardItem = ({ children }) => (
   </Card>
 );
 
-const ColumnItem = ({ label, dataLabel, icon, color, loading }) => {
+const ColumnItem = ({ tooltipTitle, label, dataLabel, icon, color, loading }) => {
   if (loading) return <Skeleton height="1rem" />;
   return (
-    <Stack textAlign="center">
-      <Typography variant="h4" color={color}>
-        {dataLabel}
-      </Typography>
-      <Stack direction="row" alignItems="center" justifyContent="center">
-        <IconButton disabled size="small">
-          {icon}
-        </IconButton>
-        <Typography variant="caption" color="text.secondary">
-          {pluralizeWord('asset', dataLabel)}
+    <Tooltip title={tooltipTitle}>
+      <Stack textAlign="center">
+        <Typography variant="h4" color={color}>
+          {dataLabel}
+        </Typography>
+        <Stack direction="row" alignItems="center" justifyContent="center">
+          <IconButton disabled size="small">
+            {icon}
+          </IconButton>
+          <Typography variant="caption" color="text.secondary">
+            {pluralizeWord('asset', dataLabel)}
+          </Typography>
+        </Stack>
+        <Typography variant="subtitle2" color="text.secondary">
+          {label}
         </Typography>
       </Stack>
-      <Typography variant="subtitle2" color="text.secondary">
-        {label}
-      </Typography>
-    </Stack>
+    </Tooltip>
   );
 };
--- frontend/src/features/Home/HomeHeader/HomeHeader.stories.js
+++ frontend/src/features/Home/HomeHeader/HomeHeader.stories.js
@@ -7,7 +7,7 @@ export default {
 
 export const HomeHeaderDefault = {
   args: {
-    categories: [
+    assetsUnderCategories: [
       {
         id: '04799e99-30a3-4c70-abbd-13dd7ed950eb',
         name: 'Groceries',
@@ -20,7 +20,7 @@ export const HomeHeaderDefault = {
         sharable_groups: ['96cf5b68-fcda-422c-8d61-8f638e2803a5'],
       },
     ],
-    maintenancePlans: [
+    assetsUnderMaintenancePlans: [
       {
         id: 'f61f101a-d848-4c5d-972c-853e7ab16866',
         name: 'Comprehensive Overhaul',
@@ -33,43 +33,14 @@ export const HomeHeaderDefault = {
         sharable_groups: ['96cf5b68-fcda-422c-8d61-8f638e2803a5'],
       },
     ],
-    assets: [
-      {
-        id: '',
-        name: 'Dog food',
-        description: '6 pounds of food bought from tractor supply',
-        price: 96,
-        status: '',
-        barcode: '',
-        sku: '',
-        quantity: 1,
-        location: '',
-        storage_location_id: '',
-        is_returnable: false,
-        return_location: 'amazon',
-        returntime: '2024-10-30T06:00:00Z',
-        max_weight: '',
-        min_weight: '',
-        max_height: '',
-        min_height: '',
-        associated_image_url: '',
-        created_at: '0001-01-01T00:00:00Z',
-        created_by: '',
-        creator_name: '',
-        updated_at: '0001-01-01T00:00:00Z',
-        updated_by: '',
-        updator: '',
-        bought_at: 'Walmart',
-        sharable_groups: null,
-      },
-    ],
+    assetsPastDue: ['Dog food'],
   },
 };
 
 export const HomeHeaderEmptyData = {
   args: {
-    categories: [],
-    maintenancePlans: [],
-    assets: [],
+    assetsUnderCategories: [],
+    assetsUnderMaintenancePlans: [],
+    assetsPastDue: [],
   },
 };
